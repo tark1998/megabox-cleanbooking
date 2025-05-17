5a6fb71a08e9f661bb4f586acdd000d7a2a0f5ee
tark1998 <tark1998@gmail.com>
2025-05-17 22:33:23 +0900

    edit description

--- code.js
+++ code.js
@@ -2,7 +2,7 @@
 // @name         megabox-cleanbooking
 // @namespace    https://github.com/tark1998/megabox-cleanbooking
 // @version      2024-03-07
-// @description  try to take over the world!
+// @description  clean the movie list in megabox booking
 // @author       You
 // @match        *://megabox.co.kr/*/simpleBookingPage.do*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
