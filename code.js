b61693e4c174dca27f0f666f3d5977799435aaa0
tark1998 <tark1998@gmail.com>
2025-05-17 22:18:14 +0900

    Initial commit

---
+++ code.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         megabox
+// @namespace    http://tampermonkey.net/
+// @version      2024-03-07
+// @description  try to take over the world!
+// @author       You
+// @match        *://megabox.co.kr/*/simpleBookingPage.do*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    GM_addStyle(`li:has(> button.disabled){
+        display:none;
+    }`)
+})();
